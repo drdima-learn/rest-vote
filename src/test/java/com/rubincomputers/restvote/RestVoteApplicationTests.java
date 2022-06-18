18a5809188267db581f9ed90a54f8ef29fff0408
Dmitry <drdima2@gmail.com>
2022-06-18 22:06:47 +0300

    first commit

---
+++ src/test/java/com/rubincomputers/restvote/RestVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubincomputers.restvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
